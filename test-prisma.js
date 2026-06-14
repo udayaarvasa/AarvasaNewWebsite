6093ce9cecee6780d757f401135692c6304b9041
Ansh Dubey <135658059+Anshdubey47@users.noreply.github.com>
2026-06-14 21:08:39 +0530

    chore: update test-prisma.js password to Aarvasa2026

--- test-prisma.js
+++ test-prisma.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: "postgresql://postgres:Aarvasa2104@database-1.cqpc8oe8w8lz.us-east-1.rds.amazonaws.com:5432/postgres?sslmode=require"
+      url: "postgresql://postgres:Aarvasa2026@database-1.cqpc8oe8w8lz.us-east-1.rds.amazonaws.com:5432/postgres?sslmode=require"
     }
   }
 });
