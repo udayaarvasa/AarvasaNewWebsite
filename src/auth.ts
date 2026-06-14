6f7503e623cd982b12d17f9fc0f96ab4fc47f316
Ansh Dubey <135658059+Anshdubey47@users.noreply.github.com>
2026-06-15 00:19:50 +0530

    fix: allow dangerous email account linking for Google provider in next-auth

--- src/auth.ts
+++ src/auth.ts
@@ -19,6 +19,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       id: "credentials",
